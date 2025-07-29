cbae4b06b8c66ae60b6e3f9f60609f09d38ff64c
Mkoek213 <mikolaj.kolek2@gmail.com>
2025-07-29 09:46:08 +0200

    zmodyfikowałem num_wokrkers

--- tests/datamodule.py
+++ tests/datamodule.py
@@ -42,10 +42,10 @@ class MNISTDataModule(pl.LightningDataModule):
             self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
 
     def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=7)
+        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=4)
 
     def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=7)
+        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=4)
 
     def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=7)
+        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=4)
