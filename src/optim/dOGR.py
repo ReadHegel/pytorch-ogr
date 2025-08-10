664ac87292600e9b8d7dbb27b9050b43bf1546f5
Mkoek213 <mikolaj.kolek2@gmail.com>
2025-08-10 16:19:20 +0200

    Dodałem trust_factor, linear i nonlinear clipping, początek clippingu z użyciem wariancji

--- src/optim/dOGR.py
+++ src/optim/dOGR.py
@@ -11,17 +11,24 @@ from torch.optim.optimizer import ParamsT, _use_grad_for_differentiable
 from torch import Tensor
 
 
+
 class dOGR(Optimizer):
     def __init__(
         self,
         params: ParamsT,
         lr: Union[float, Tensor] = 1e-3,
         beta: float = 0.50,
+        trust_factor: float = 1.0,
         eps: float = 1e-8,
         maximize: bool = False,
         differentiable: bool = False,
-        hybrid_clipping: bool = False,
-        neg_clip_val: Optional[float] = None,
+        linear_clipping: bool = False,
+        nonlinear_clipping: bool = False,
+        var_clipping: bool = False,
+        var_fixed: float = 1.0,
+        p_norm: float = 2.0,
+        p_eps: float = 0.1,
+        neg_clip_val: float = 10.0,
     ):
         if isinstance(lr, Tensor) and lr.numel() != 1:
             raise ValueError("Tensor lr must be 1-element")
@@ -33,9 +40,15 @@ class dOGR(Optimizer):
             "lr": lr,
             "maximize": maximize,
             "beta": beta,
+            "trust_factor": trust_factor,
             "eps": eps,
             "differentiable": differentiable,
-            "hybrid_clipping": hybrid_clipping,
+            "linear_clipping": linear_clipping,
+            "nonlinear_clipping": nonlinear_clipping,
+            "var_clipping": var_clipping,
+            "var_fixed": var_fixed,
+            "p_norm": p_norm,
+            "p_eps": p_eps,
             "neg_clip_val": neg_clip_val,
         }
 
@@ -118,6 +131,7 @@ class dOGR(Optimizer):
                 grads,
                 lr=group["lr"],
                 beta=group["beta"],
+                trust_factor=group["trust_factor"],
                 eps=group["eps"],
                 mean_params=mean_params,
                 mean_grads=mean_grads,
@@ -125,7 +139,12 @@ class dOGR(Optimizer):
                 d_grads_params=d_grads_params,
                 mean=mean,
                 maximize=group["maximize"],
-                hybrid_clipping=group["hybrid_clipping"],
+                linear_clipping=group["linear_clipping"],
+                nonlinear_clipping=group["nonlinear_clipping"],
+                var_clipping=group["var_clipping"],
+                var_fixed=group["var_fixed"],
+                p_norm=group["p_norm"],
+                p_eps=group["p_eps"],
                 neg_clip_val=group["neg_clip_val"],
             )
 
@@ -137,6 +156,7 @@ def dogr(
     grads: list[Tensor],
     lr: float,
     beta: float,
+    trust_factor: float,
     eps: float,
     mean_params: list[Tensor],
     mean_grads: list[Tensor],
@@ -144,8 +164,13 @@ def dogr(
     d_grads_params: list[Tensor],
     mean: list[Tensor],
     maximize: bool,
-    hybrid_clipping: bool,
-    neg_clip_val: Optional[float],
+    linear_clipping: bool,
+    nonlinear_clipping: bool,
+    var_clipping: bool,
+    var_fixed: float,
+    p_norm: float,
+    p_eps: float,
+    neg_clip_val: float,
 ):
     for i, param in enumerate(params):
         grad = grads[i] if not maximize else -grads[i]
@@ -177,16 +202,9 @@ def dogr(
         H_sign = torch.sign(d_grads_params[i])
         H = d_grads_params[i] / (d_params_params[i] + eps)
 
-        # theta = theta + sing(H)(p - param)
-        if not hybrid_clipping:
-            param.add_(
-                torch.where(
-                    H_sign == 0,
-                    - grad * lr,
-                    - 1 / torch.maximum(torch.abs(H) * 1.5, 5 * torch.ones_like(H)) * mean_grads[i],
-                ),
-            )
-        else:
+        
+        
+        if linear_clipping:
             normal_step = - (1 / torch.maximum(torch.abs(H) * 1.5, 5 * torch.ones_like(H))) * mean_grads[i]
             aggressive_clip_step = - (1 / torch.maximum(torch.abs(H) * 1.5, neg_clip_val * torch.ones_like(H))) * mean_grads[i]
 
@@ -200,6 +218,56 @@ def dogr(
                         -grad * lr
                     )
                 )
+            )
+        
+        # 1 / (|H|^p + p_eps^p)^(1/p) * mean_grads[i]
+        elif nonlinear_clipping:
+            normal_step = - (1 / torch.maximum(torch.abs(H) * 1.5, 5 * torch.ones_like(H))) * mean_grads[i]
+            
+        
+            abs_H = torch.abs(H)
+
+            denominator = (abs_H.pow(p_norm) + p_eps**p_norm).pow(1.0 / p_norm)
+            nonlinear_clip_step = - (1 / denominator) * mean_grads[i]
+
+            param.add_(
+                torch.where(
+                    H > 0,
+                    normal_step,
+                    torch.where(
+                        H < 0,
+                        nonlinear_clip_step, 
+                        -grad * lr
+                    )
+                ), alpha=trust_factor
+            )
+
+        elif var_clipping:
+            normal_step = - (1 / torch.maximum(torch.abs(H) * 1.5, 5 * torch.ones_like(H))) * mean_grads[i]
+            denominator = torch.abs(H) + torch.sqrt(H**2 + var_fixed)
+            var_clip_step = - (2 / (denominator + eps)) * mean_grads[i]
+            param.add_(
+                torch.where(
+                    H > 0,
+                    normal_step,
+                    torch.where(
+                        H < 0,
+                        var_clip_step, 
+                        -grad * lr
+                    )
+                ), alpha=trust_factor
+            )
+
+
+
+        # theta = theta + sing(H)(p - param)
+        else:
+            param.add_(
+                torch.where(
+                    H_sign == 0,
+                    - grad * lr,
+                    - 1 / torch.maximum(torch.abs(H) * 1.5, 5 * torch.ones_like(H)) * mean_grads[i],
+                ),
             )
 
         # for debug
